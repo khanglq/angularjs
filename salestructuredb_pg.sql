0b38685849883ddf12f2634e6daa540041a5031d
khanglq <khanglq2011@gmail.com>
2020-07-09 10:06:54 +0700

    Update salestructuredb_pg.sql

--- salestructuredb_pg.sql
+++ salestructuredb_pg.sql
@@ -11,12 +11,12 @@ CREATE TABLE "Fact_Transaction" (
   "Value_Date" date,
   "Expire_Date" date,
   "Maturity_Date" date,
-  "Payment_Date" varchar2(25),
+  "Payment_Date" varchar(25),
   "Amount" numeric(20,4),
   "Quantity" int,
-  "Indiv_Org_Flag" number NOT NULL,
-  "Day_Count_Convention" varchar2(500),
-  "Contract_ID_Ref" number
+  "Indiv_Org_Flag" numeric NOT NULL,
+  "Day_Count_Convention" varchar(500),
+  "Contract_ID_Ref" numeric
 );
 
 CREATE TABLE "Dim_Cust_ID" (
@@ -110,6 +110,111 @@ CREATE TABLE "Dim_Day_Convention" (
   "End_Date" date
 );
 
-ALTER TABLE "Fact_Transaction" ADD FOREIGN KEY ("FK_Cust_ID") REFERENCES "Dim_Cust_ID" ("PK_ID");
+CREATE TABLE "Dim_Sale_Person" (
+  "PK_ID" numeric PRIMARY KEY,
+  "Sale_ID" varchar(60) NOT NULL,
+  "FK_Employee_ID" numeric NOT NULL,
+  "System_Code" varchar(25)
+);
 
-ALTER TABLE "Fact_Transaction" ADD FOREIGN KEY ("FK_Contract_ID") REFERENCES "Dim_Contract_ID" ("PK_ID");
+CREATE TABLE "Dim_Sale_Person_Customer" (
+  "PK_ID" numeric PRIMARY KEY,
+  "FK_Sale_ID" numeric NOT NULL,
+  "Product_Code" varchar(60) NOT NULL,
+  "FK_Cust_ID" numeric NOT NULL,
+  "Start_Date" date,
+  "End_Date" date,
+  "Created_Date" date,
+  "Updated_Date" date,
+  "Created_By" varchar(25),
+  "Updated_By" varchar(25)
+);
+
+CREATE TABLE "Fact_Product_Revenue" (
+  "PK_ID" numeric PRIMARY KEY,
+  "Product_Code" varchar(60) NOT NULL,
+  "FK_Sale_ID" numeric NOT NULL,
+  "Process_Date" date,
+  "Amount" numeric(20,4),
+  "Month" int,
+  "Quarter" int,
+  "Year" int
+);
+
+CREATE TABLE "Provision_Product_Revenue_NET" (
+  "PK_ID" numeric PRIMARY KEY,
+  "Product_Code" varchar(60) NOT NULL,
+  "FK_Sale_ID" numeric NOT NULL,
+  "Process_Date" date,
+  "Amount" numeric(20,4),
+  "Total_Balance" numeric(20,4),
+  "Target_Current_Period" numeric(20,4),
+  "Target_Prev_Period" numeric(20,4),
+  "Amount_Prev_Period" numeric(20,4),
+  "Accumulative_Target_Achieved_Pct" numeric(20,4),
+  "Month" int,
+  "Quarter" int,
+  "Year" int
+);
+
+CREATE TABLE "Fact_KPI_Product_Result" (
+  "PK_ID" numeric PRIMARY KEY,
+  "Session_ID" numeric,
+  "Product_Code" varchar(60) NOT NULL,
+  "FK_Sale_ID" numeric NOT NULL,
+  "Process_Date" date,
+  "Amount" numeric(20,4),
+  "Total_Balance" numeric(20,4),
+  "Target_Current_Period" numeric(20,4),
+  "Target_Prev_Period" numeric(20,4),
+  "Amount_Prev_Period" numeric(20,4),
+  "Accumulative_Target_Achieved_Pct" numeric(20,4),
+  "Month" int,
+  "Quarter" int,
+  "Year" int,
+  "KPI_PCT" numeric,
+  "KPI_Result" varchar(30),
+  "No_Of_Run" int
+);
+
+CREATE TABLE "Fact_KPI_Reconcile_Result" (
+  "PK_ID" numeric PRIMARY KEY,
+  "Session_ID" numeric,
+  "FK_Sale_ID" numeric NOT NULL,
+  "Process_Date" date,
+  "Amount" numeric(20,4),
+  "Total_Balance" numeric(20,4),
+  "Target_Current_Period" numeric(20,4),
+  "Target_Prev_Period" numeric(20,4),
+  "Amount_Prev_Period" numeric(20,4),
+  "Accumulative_Target_Achieved_Pct" numeric(20,4),
+  "Month" int,
+  "Quarter" int,
+  "Year" int,
+  "KPI_PCT" numeric,
+  "KPI_Result" varchar(30),
+  "No_Of_Run" int,
+  "Note" varchar(4000),
+  "Attachment" varchar(300)
+);
+
+CREATE TABLE "Fact_KPI_Result" (
+  "PK_ID" numeric PRIMARY KEY,
+  "Session_ID" numeric,
+  "FK_Sale_ID" numeric NOT NULL,
+  "Process_Date" date,
+  "Amount" numeric(20,4),
+  "Total_Balance" numeric(20,4),
+  "Target_Current_Period" numeric(20,4),
+  "Target_Prev_Period" numeric(20,4),
+  "Amount_Prev_Period" numeric(20,4),
+  "Accumulative_Target_Achieved_Pct" numeric(20,4),
+  "Month" int,
+  "Quarter" int,
+  "Year" int,
+  "KPI_PCT" numeric,
+  "KPI_Result" varchar(30),
+  "No_Of_Run" int,
+  "Note" varchar(4000),
+  "Attachment" varchar(300)
+);
