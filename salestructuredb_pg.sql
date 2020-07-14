e15ae2b4ff8c6da598c2adcee109d48379a4b1a5
khanglq <khanglq2011@gmail.com>
2020-07-14 09:25:00 +0700

    Update salestructuredb_pg.sql

--- salestructuredb_pg.sql
+++ salestructuredb_pg.sql
@@ -1,11 +1,10 @@
 
--- db structure
 CREATE TABLE "Fact_Transaction" (
-  "PK_ID" numeric PRIMARY KEY,
-  "FK_Contract_ID" numeric NOT NULL,
+  "PK_ID" bigint PRIMARY KEY,
+  "FK_Contract_ID" bigint NOT NULL,
   "Product_Code" varchar(60) NOT NULL,
-  "FK_Sale_ID" numeric NOT NULL,
-  "FK_Cust_ID" numeric NOT NULL,
+  "FK_Sale_ID" bigint NOT NULL,
+  "FK_Cust_ID" bigint NOT NULL,
   "Product_Type" varchar(50),
   "Product_Sub_Type" varchar(50),
   "Transaction_Type" varchar(50),
@@ -14,27 +13,27 @@ CREATE TABLE "Fact_Transaction" (
   "Expire_Date" date,
   "Maturity_Date" date,
   "Payment_Date" varchar(25),
-  "Amount" numeric(20,4),
+  "Amount" bigint,
   "Quantity" int,
-  "Indiv_Org_Flag" numeric NOT NULL,
+  "Indiv_Org_Flag" bigint NOT NULL,
   "Day_Count_Convention" varchar(500),
-  "Contract_ID_Ref" numeric
+  "Contract_ID_Ref" bigint
 );
 
 CREATE TABLE "Dim_Cust_ID" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Cust_No" varchar(100) NOT NULL,
   "System_Code" varchar(100) NOT NULL
 );
 
 CREATE TABLE "Dim_Contract_ID" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Contract_No" varchar(100) NOT NULL,
   "System_Code" varchar(100) NOT NULL
 );
 
 CREATE TABLE "Dim_Branch" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Name" varchar(200) NOT NULL,
   "Desc" varchar(4000) NOT NULL,
   "Start_Date" date,
@@ -42,7 +41,7 @@ CREATE TABLE "Dim_Branch" (
 );
 
 CREATE TABLE "Dim_Department" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Name" varchar(200) NOT NULL,
   "Desc" varchar(4000) NOT NULL,
   "Start_Date" date,
@@ -50,7 +49,7 @@ CREATE TABLE "Dim_Department" (
 );
 
 CREATE TABLE "Dim_Sale_Channel" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Name" varchar(200) NOT NULL,
   "Desc" varchar(4000) NOT NULL,
   "Start_Date" date,
@@ -58,15 +57,15 @@ CREATE TABLE "Dim_Sale_Channel" (
 );
 
 CREATE TABLE "Dim_Employee" (
-  "PK_ID" numeric PRIMARY KEY,
-  "Name" numeric NOT NULL,
+  "PK_ID" bigint PRIMARY KEY,
+  "Name" bigint NOT NULL,
   "Address" varchar(400) NOT NULL,
   "Phone_No" varchar(50) NOT NULL,
   "Probation_Date" date NOT NULL,
   "Sign_Contract_Date" date,
   "Exit_Date" date,
-  "FK_Branch_ID" numeric,
-  "FK_Department_ID" numeric,
+  "FK_Branch_ID" bigint,
+  "FK_Department_ID" bigint,
   "Created_Date" date,
   "Updated_Date" date,
   "Created_By" varchar(25),
@@ -74,8 +73,8 @@ CREATE TABLE "Dim_Employee" (
 );
 
 CREATE TABLE "Dim_Employee_Leave" (
-  "PK_ID" numeric PRIMARY KEY,
-  "FK_Employee_ID" numeric NOT NULL,
+  "PK_ID" bigint PRIMARY KEY,
+  "FK_Employee_ID" bigint NOT NULL,
   "Leave_Type" varchar(50),
   "Note" varchar(4000),
   "From_Date" date,
@@ -87,7 +86,7 @@ CREATE TABLE "Dim_Employee_Leave" (
 );
 
 CREATE TABLE "Dim_Product_Category" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Name" varchar(200) NOT NULL,
   "Desc" varchar(4000),
   "Start_Date" date,
@@ -95,8 +94,8 @@ CREATE TABLE "Dim_Product_Category" (
 );
 
 CREATE TABLE "Dim_Product_Code" (
-  "PK_ID" numeric PRIMARY KEY,
-  "FK_Product_Cat" numeric NOT NULL,
+  "PK_ID" bigint PRIMARY KEY,
+  "FK_Product_Cat" bigint NOT NULL,
   "Product_Code" varchar(60) UNIQUE NOT NULL,
   "Desc" varchar(4000),
   "Start_Date" date,
@@ -104,7 +103,7 @@ CREATE TABLE "Dim_Product_Code" (
 );
 
 CREATE TABLE "Dim_Day_Convention" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Product_Code" varchar(60) UNIQUE NOT NULL,
   "Code" varchar(60),
   "Desc" varchar(4000),
@@ -113,17 +112,17 @@ CREATE TABLE "Dim_Day_Convention" (
 );
 
 CREATE TABLE "Dim_Sale_Person" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Sale_ID" varchar(60) NOT NULL,
-  "FK_Employee_ID" numeric NOT NULL,
+  "FK_Employee_ID" bigint NOT NULL,
   "System_Code" varchar(25)
 );
 
 CREATE TABLE "Dim_Sale_Person_Customer" (
-  "PK_ID" numeric PRIMARY KEY,
-  "FK_Sale_ID" numeric NOT NULL,
+  "PK_ID" bigint PRIMARY KEY,
+  "FK_Sale_ID" bigint NOT NULL,
   "Product_Code" varchar(60) NOT NULL,
-  "FK_Cust_ID" numeric NOT NULL,
+  "FK_Cust_ID" bigint NOT NULL,
   "Start_Date" date,
   "End_Date" date,
   "Created_Date" date,
@@ -133,68 +132,68 @@ CREATE TABLE "Dim_Sale_Person_Customer" (
 );
 
 CREATE TABLE "Fact_Product_Revenue" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Product_Code" varchar(60) NOT NULL,
-  "FK_Sale_ID" numeric NOT NULL,
-  "FK_Contract_ID" numeric NOT NULL,
+  "FK_Sale_ID" bigint NOT NULL,
+  "FK_Contract_ID" bigint NOT NULL,
   "Process_Date" date,
-  "Amount" numeric(20,4),
+  "Amount" bigint,
   "Month" int,
   "Quarter" int,
   "Year" int
 );
 
 CREATE TABLE "Provision_Product_Revenue_NET" (
-  "PK_ID" numeric PRIMARY KEY,
+  "PK_ID" bigint PRIMARY KEY,
   "Product_Code" varchar(60) NOT NULL,
-  "FK_Sale_ID" numeric NOT NULL,
+  "FK_Sale_ID" bigint NOT NULL,
   "Process_Date" date,
-  "Amount" numeric(20,4),
-  "Total_Balance" numeric(20,4),
-  "Target_Current_Period" numeric(20,4),
-  "Target_Prev_Period" numeric(20,4),
-  "Amount_Prev_Period" numeric(20,4),
-  "Accumulative_Target_Achieved_Pct" numeric(20,4),
+  "Amount" bigint,
+  "Total_Balance" bigint,
+  "Target_Current_Period" bigint,
+  "Target_Prev_Period" bigint,
+  "Amount_Prev_Period" bigint,
+  "Accumulative_Target_Achieved_Pct" bigint,
   "Month" int,
   "Quarter" int,
   "Year" int
 );
 
 CREATE TABLE "Fact_KPI_Product_Result" (
-  "PK_ID" numeric PRIMARY KEY,
-  "Session_ID" numeric,
+  "PK_ID" bigint PRIMARY KEY,
+  "Session_ID" bigint,
   "Product_Code" varchar(60) NOT NULL,
-  "FK_Sale_ID" numeric NOT NULL,
+  "FK_Sale_ID" bigint NOT NULL,
   "Process_Date" date,
-  "Amount" numeric(20,4),
-  "Total_Balance" numeric(20,4),
-  "Target_Current_Period" numeric(20,4),
-  "Target_Prev_Period" numeric(20,4),
-  "Amount_Prev_Period" numeric(20,4),
-  "Accumulative_Target_Achieved_Pct" numeric(20,4),
+  "Amount" bigint,
+  "Total_Balance" bigint,
+  "Target_Current_Period" bigint,
+  "Target_Prev_Period" bigint,
+  "Amount_Prev_Period" bigint,
+  "Accumulative_Target_Achieved_Pct" bigint,
   "Month" int,
   "Quarter" int,
   "Year" int,
-  "KPI_PCT" numeric,
+  "KPI_PCT" bigint,
   "KPI_Result" varchar(30),
   "No_Of_Run" int
 );
 
 CREATE TABLE "Fact_KPI_Reconcile_Result" (
-  "PK_ID" numeric PRIMARY KEY,
-  "Session_ID" numeric,
-  "FK_Sale_ID" numeric NOT NULL,
+  "PK_ID" bigint PRIMARY KEY,
+  "Session_ID" bigint,
+  "FK_Sale_ID" bigint NOT NULL,
   "Process_Date" date,
-  "Amount" numeric(20,4),
-  "Total_Balance" numeric(20,4),
-  "Target_Current_Period" numeric(20,4),
-  "Target_Prev_Period" numeric(20,4),
-  "Amount_Prev_Period" numeric(20,4),
-  "Accumulative_Target_Achieved_Pct" numeric(20,4),
+  "Amount" bigint,
+  "Total_Balance" bigint,
+  "Target_Current_Period" bigint,
+  "Target_Prev_Period" bigint,
+  "Amount_Prev_Period" bigint,
+  "Accumulative_Target_Achieved_Pct" bigint,
   "Month" int,
   "Quarter" int,
   "Year" int,
-  "KPI_PCT" numeric,
+  "KPI_PCT" bigint,
   "KPI_Result" varchar(30),
   "No_Of_Run" int,
   "Note" varchar(4000),
@@ -202,42 +201,58 @@ CREATE TABLE "Fact_KPI_Reconcile_Result" (
 );
 
 CREATE TABLE "Fact_KPI_Result" (
-  "PK_ID" numeric PRIMARY KEY,
-  "Session_ID" numeric,
-  "FK_Sale_ID" numeric NOT NULL,
+  "PK_ID" bigint PRIMARY KEY,
+  "Session_ID" bigint,
+  "FK_Sale_ID" bigint NOT NULL,
   "Process_Date" date,
-  "Amount" numeric(20,4),
-  "Total_Balance" numeric(20,4),
-  "Target_Current_Period" numeric(20,4),
-  "Target_Prev_Period" numeric(20,4),
-  "Amount_Prev_Period" numeric(20,4),
-  "Accumulative_Target_Achieved_Pct" numeric(20,4),
+  "Amount" bigint,
+  "Total_Balance" bigint,
+  "Target_Current_Period" bigint,
+  "Target_Prev_Period" bigint,
+  "Amount_Prev_Period" bigint,
+  "Accumulative_Target_Achieved_Pct" bigint,
   "Month" int,
   "Quarter" int,
   "Year" int,
-  "KPI_PCT" numeric,
+  "KPI_PCT" bigint,
   "KPI_Result" varchar(30),
   "No_Of_Run" int,
   "Note" varchar(4000),
   "Attachment" varchar(300)
 );
 
--- table relations
 ALTER TABLE "Fact_Transaction" ADD FOREIGN KEY ("FK_Cust_ID") REFERENCES "Dim_Cust_ID" ("PK_ID");
+
 ALTER TABLE "Dim_Product_Code" ADD FOREIGN KEY ("FK_Product_Cat") REFERENCES "Dim_Product_Category" ("PK_ID");
+
 ALTER TABLE "Dim_Day_Convention" ADD FOREIGN KEY ("Product_Code") REFERENCES "Dim_Product_Code" ("Product_Code");
+
 ALTER TABLE "Fact_Transaction" ADD FOREIGN KEY ("Product_Code") REFERENCES "Dim_Product_Code" ("Product_Code");
+
 ALTER TABLE "Dim_Sale_Person_Customer" ADD FOREIGN KEY ("FK_Sale_ID") REFERENCES "Dim_Sale_Person" ("PK_ID");
+
 ALTER TABLE "Dim_Sale_Person_Customer" ADD FOREIGN KEY ("Product_Code") REFERENCES "Dim_Product_Code" ("Product_Code");
+
 ALTER TABLE "Fact_Product_Revenue" ADD FOREIGN KEY ("FK_Contract_ID") REFERENCES "Dim_Contract_ID" ("PK_ID");
+
 ALTER TABLE "Fact_Product_Revenue" ADD FOREIGN KEY ("Product_Code") REFERENCES "Dim_Product_Code" ("Product_Code");
+
 ALTER TABLE "Fact_Product_Revenue" ADD FOREIGN KEY ("FK_Sale_ID") REFERENCES "Dim_Sale_Person" ("PK_ID");
+
 ALTER TABLE "Fact_Transaction" ADD FOREIGN KEY ("FK_Contract_ID") REFERENCES "Dim_Contract_ID" ("PK_ID");
+
 ALTER TABLE "Provision_Product_Revenue_NET" ADD FOREIGN KEY ("FK_Sale_ID") REFERENCES "Dim_Sale_Person" ("PK_ID");
+
 ALTER TABLE "Provision_Product_Revenue_NET" ADD FOREIGN KEY ("Product_Code") REFERENCES "Dim_Product_Code" ("Product_Code");
+
 ALTER TABLE "Fact_KPI_Reconcile_Result" ADD FOREIGN KEY ("PK_ID") REFERENCES "Fact_KPI_Product_Result" ("PK_ID");
-ALTER TABLE "Fact_KPI_Product_Result" ADD FOREIGN KEY ("FK_Sale_ID") REFERENCES "Fact_KPI_Result" ("FK_Sale_ID");
+
+--ALTER TABLE "Fact_KPI_Product_Result" ADD FOREIGN KEY ("FK_Sale_ID") REFERENCES "Fact_KPI_Result" ("FK_Sale_ID");
+
 ALTER TABLE "Dim_Sale_Person" ADD FOREIGN KEY ("FK_Employee_ID") REFERENCES "Dim_Employee" ("PK_ID");
+
 ALTER TABLE "Dim_Employee_Leave" ADD FOREIGN KEY ("FK_Employee_ID") REFERENCES "Dim_Employee" ("PK_ID");
+
 ALTER TABLE "Dim_Employee" ADD FOREIGN KEY ("FK_Branch_ID") REFERENCES "Dim_Branch" ("PK_ID");
+
 ALTER TABLE "Dim_Employee" ADD FOREIGN KEY ("FK_Department_ID") REFERENCES "Dim_Department" ("PK_ID");
